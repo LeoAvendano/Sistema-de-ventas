9acaddd21d48bc6fb49e8b9b4620ffef5b93b4bc
maurocastro29 <mm.castro.29@mail.com>
2022-11-02 18:01:45 -0500

    Creacion de la clase VentanaLogin e implementacion de inicio de sesion

--- login.py
+++ login.py
@@ -5,60 +5,72 @@ from Conexion import *
 
 import pymysql
 
-def vista_login():
-    global pantalla
-    pantalla = Tk()
-    pantalla.geometry("350x400")
-    pantalla.title("Login")
-    Label(text="INICIAR SESION", bg="navy", fg="white", width="100", height="3", font=("Calibri", 15)).pack()
-    Label(text="").pack()
-    global usuario_verify
-    global password_verify
-    usuario_verify = StringVar()
-    password_verify = StringVar()
+class VentanaLogin(Frame):
 
-    global usuario_entry
-    global password_entry
-    Label(text="Usuario", font=("Calibri", 13)).pack()
-    usuario_entry = Entry(pantalla,  width=25, font=("Calibri", 12), textvariable=usuario_verify)
-    usuario_entry.pack()
-    Label(pantalla).pack()
-    Label(text="Contraseña", font=("Calibri", 13)).pack()
-    password_entry = Entry(pantalla, show="*", width=25, font=("Calibri", 12), textvariable=password_verify)
-    password_entry.pack()
-    Label(pantalla).pack()
-    Label(pantalla).pack()
-    Button(pantalla, text="Ingresar", height="2", width="15", font=("Calibri", 15), bg="BLUE", command=login).pack()
-    pantalla.mainloop()
+    def __init__(self, master = None):
+        super().__init__(master)
+        master.geometry("350x400")
+        self.master = master
+        Label(text="INICIAR SESION", bg="navy", fg="white", width="100", height="3", font=("Calibri", 15)).pack()
+        Label(text="").pack()
+        global usuario_verify
+        global password_verify
+        usuario_verify = StringVar()
+        password_verify = StringVar()
+        global usuario_entry
+        global password_entry
+        Label(text="Usuario", font=("Calibri", 13)).pack()
+        usuario_entry = Entry(master, width=25, font=("Calibri", 12), textvariable=usuario_verify)
+        usuario_entry.pack()
+        Label(master).pack()
+        Label(text="Contraseña", font=("Calibri", 13)).pack()
+        password_entry = Entry(master, show="*", width=25, font=("Calibri", 12), textvariable=password_verify)
+        password_entry.pack()
+        Label(master).pack()
+        Label(master).pack()
+        Button(master, text="Ingresar", height="2", width="15", font=("Calibri", 15), bg="BLUE", command=self.login).pack()
+        self.pack()
+        self.create_widgets()
 
-def login():
-    if validarDatos() == False:
-        messagebox.showinfo(message="Faltan campos por rellenar", title="Aviso")
-        return
-    con = Conexion()
-    bd = con.conectar()
-    fcursor = bd.cursor()
-    sql = "SELECT * FROM usuario WHERE " \
-            "usuario = '"+usuario_verify.get()+"' and " \
-            "clave = '"+password_verify.get()+"' and " \
-            "estado = 1"
-    try:
-        fcursor.execute(sql)
-        if(fcursor.fetchall()):
-            messagebox.showinfo(message="Usuario y contraseña correctos", title="Aviso")
-            bd.commit()
-        else:
-            messagebox.showinfo(message="Usuario y/o contraseña incorrectos", title="Aviso")
+    def create_widgets(self):
+        pass
+
+    def login(self):
+        if self.validar_datos_login() == False:
+            messagebox.showinfo(message="Faltan campos por rellenar", title="Aviso")
+            return
+        con = Conexion()
+        bd = con.conectar()
+        fcursor = bd.cursor()
+        sql = "SELECT * FROM usuario WHERE " \
+              "usuario = '" + usuario_verify.get() + "' and " \
+                                                     "clave = '" + password_verify.get() + "' and " \
+                                                                                           "estado = 1"
+        try:
+            fcursor.execute(sql)
+            if (fcursor.fetchall()):
+                messagebox.showinfo(message="Usuario y contraseña correctos", title="Aviso")
+                bd.commit()
+            else:
+                messagebox.showinfo(message="Usuario y/o contraseña incorrectos", title="Aviso")
+                bd.rollback()
+        except:
             bd.rollback()
-    except:
-        bd.rollback()
-        messagebox.showinfo(message="Fallo en la conexion", title="Aviso")
-    bd.close()
+            messagebox.showinfo(message="Fallo en la conexion", title="Aviso")
+        bd.close()
 
-def validarDatos():
-    usuario = usuario_verify.get()
-    passw = password_verify.get()
-    return len(usuario)>0 and len(passw)>0
+    def validar_datos_login(self):
+        usuario = usuario_verify.get()
+        passw = password_verify.get()
+        return len(usuario) > 0 and len(passw) > 0
+
+
+
+def main():
+    root = Tk()
+    root.wm_title("Inicio de sesion")
+    app = VentanaLogin(root)
+    app.mainloop()
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from login import *
 
 
-
 if __name__ == "__main__":
-    vista_login()
+    main()
 
 
